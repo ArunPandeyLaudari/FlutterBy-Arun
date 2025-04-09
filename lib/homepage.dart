7821501c420ca93afe26e27fee4369d7e707e246
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-04-09 18:45:50 +0545

    Child,Text,Center Widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,17 +7,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Arun First Flutter Projects",
-          style: TextStyle(color: const Color.fromARGB(255, 10, 143, 10)),
-        ),
+        title: Text("This is Title", style: TextStyle(color: Colors.red)),
+        backgroundColor: Colors.black,
       ),
+
       body: Center(
         child: Container(
-          width: 100,
-          height: 100,
+          height: 300,
+          width: 300,
           color: Colors.red,
-          child: Text("Hello World"),
+
+          child: Center(
+            child: Text(
+              "Arun",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+          ),
         ),
       ),
     );
